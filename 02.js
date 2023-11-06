de15ea0a26467bd4d13443082f29ec42b90c0906
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2023-11-07 00:00:04 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,19 @@
+// DATATYPES AND ECMA
+console.log("DATATYPES IN JS")
+// string
+// int
+// boolean
+// null
+// undefined
+// symbol
+// object
+
+let name = "Yash"
+age = 18
+Male = true
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof Male)
---
+++ basic.js
@@ -0,0 +1,14 @@
+// syntax to print any thing in JS
+console.log("Hello JavaScript")
+
+
+// VARIABLE DECLARATION
+const UserID = "696969"
+let SurName = "Yash Vishwakarma"  // let >> var for variable declaration
+var Name = "Yash"
+MiddleName = "Satyaprakash"
+
+//console.log(UserID); //using this we can print one variable at a time
+
+console.table([UserID, Name, SurName, MiddleName]);
+
