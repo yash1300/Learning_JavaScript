1615a379055d7187b51575a8c7db56898c4379ff
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2023-11-11 13:35:34 +0530

    Add files via upload

---
+++ 01_basics/08.js
@@ -0,0 +1,17 @@
+// Date and time moule in JS
+
+const taarik = new Date()
+console.log(taarik);     // all different methods of date and time module
+console.log(taarik.getTime());
+console.log(taarik.getFullYear());
+console.log(taarik.getTime());
+console.log(taarik.toDateString());
+console.log(taarik.toISOString());
+console.log(taarik.toLocaleDateString());
+console.log(taarik.toLocaleString());
+
+let mydate = new Date(2023, 2, 13) // (year, month, date)
+console.log(mydate);
+console.log(mydate.toLocaleDateString());
+
+// similarly there are many more methods can try and learn as per need 
