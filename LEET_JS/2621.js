7f46d1c0f018a6f5c52763e4cb338e354161f30e
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2024-01-11 23:59:59 +0530

    Add files via upload

---
+++ LEET_JS/2621.js
@@ -0,0 +1,15 @@
+// 2621. Sleep
+
+
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+* let t = Date.now()
+* sleep(100).then(() => console.log(Date.now() - t)) // 100
+*/
